a5d1ff925e49dbb06dadf11c739a6a05d15b2e0a
notastay <notastay@gmail.com>
2024-11-26 12:09:42 +0100

    26/11/24 12:09

--- src/Tema_7/Ej8_cadenas.java
+++ src/Tema_7/Ej8_cadenas.java
@@ -8,8 +8,14 @@ public class Ej8_cadenas {
         System.out.println("Dame 1 cadena y un caracter");
         String cadena=tecl.nextLine();
         String caracter=tecl.nextLine();
+        int contadorCaracter=0;
         for (int i = 0; i < cadena.length(); i++) {
-            
+            String loop=cadena.substring(i,i+1);
+            if (loop.equals(caracter)) {
+                contadorCaracter++;
+            }
         }
+        System.out.println("Hay "+contadorCaracter+" "+caracter+"'s en tu cadena");
+        tecl.close();
     }
 }
---
+++ src/Tema_7/Ej9_cadenas.java
@@ -0,0 +1,7 @@
+package Tema_7;
+
+public class Ej9_cadenas {
+    public static void main(String[] args) {
+        
+    }
+}
