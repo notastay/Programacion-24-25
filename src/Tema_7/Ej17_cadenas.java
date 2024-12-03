d91279047329df2070e73fc571e22c277e8e6db9
notastay <notastay@gmail.com>
2024-12-03 09:43:58 +0100

    9:43

---
+++ src/Tema_7/Ej17_cadenas.java
@@ -0,0 +1,10 @@
+package Tema_7;
+
+import java.util.Scanner;
+
+public class Ej17_cadenas {
+    public static void main(String[] args) {
+        Scanner tecl=new Scanner(System.in);
+        
+    }
+}
