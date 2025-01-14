b99f2ec3d0bde1832571e76575ad4f0ebf10a5fc
notastay <notastay@gmail.com>
2025-01-14 08:42:57 +0100

    14/01

--- nadia_henrich/src/main/java/tema_8/Vectores1.java
+++ nadia_henrich/src/main/java/tema_8/Vectores1.java
@@ -1,5 +1,12 @@
 package tema_8;
 
 public class Vectores1 {
+    public static void main(String[] args) {
+        Scanner tecl=new Scanner(System.in);
+        int numeros= new int[10];
+        for (int cadaNum : numeros) {
+            System.out.println("Dime un numero hasta rellenar el array");
+        }
+    }
     
 }
