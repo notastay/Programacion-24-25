1a61bc448c830f7890825a0997ce0226d5457498
notastay <notastay@gmail.com>
2025-01-20 09:52:02 +0100

    20/01/25 9:50

--- nadia_henrich/src/main/java/tema_8/Vectores16.java
+++ nadia_henrich/src/main/java/tema_8/Vectores16.java
@@ -1,5 +1,19 @@
 package tema_8;
 
+import java.util.Arrays;
+
 public class Vectores16 {
-    // unu
+    public static void main(String[] args) {
+        int[] secuencia= new int[55];
+        int pos=0;
+        for (int i = 1; i <= 10; i++) {
+            int fin=pos+i;
+            Arrays.fill(secuencia, pos, fin, i);
+            pos=fin;
+            
+        }
+        for (int i = 0; i < secuencia.length; i++) {
+            System.out.println(secuencia[i]);
+        }
+    }
 }
