73b009e48df6b701bc72eb77b350676df6d8f3c7
notastay <151544079+notastay@users.noreply.github.com>
2025-01-27 21:32:53 +0100

    27/01/2025

--- nadia_henrich/src/main/java/tema_8/Ingredientes.java
+++ nadia_henrich/src/main/java/tema_8/Ingredientes.java
@@ -4,4 +4,50 @@ public class Ingredientes {
     private String nombre;
     private int cantidad;
     private String unidad;
+
+    
+    public Ingredientes(String nombre, float cantidad, String unidad) {
+        this.nombre = nombre;
+        this.cantidad = Math.round(cantidad);
+        this.unidad = unidad;
+    }
+
+    //getters y setters generados automaticamente
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+    public String getUnidad() {
+        return unidad;
+    }
+
+
+    public void setUnidad(String unidad) {
+        this.unidad = unidad;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Ingredientes [nombre=" + nombre + ", cantidad=" + cantidad + " " + unidad + "]";
+    }
+    
+
+    
 }
--- nadia_henrich/src/main/java/tema_8/Receta.java
+++ nadia_henrich/src/main/java/tema_8/Receta.java
@@ -1,8 +1,72 @@
 package tema_8;
 
+import java.util.Optional;
+
 public class Receta {
     private String nombre;
     private String elaboracion;
     private int duracion;
-    private Ingrediente[] ingredientes;
+    private Ingredientes[] ingredientes;
+
+    public Receta(String nombre, String elaboracion, int duracion, int canidadIngredientes) {
+        this.nombre = nombre;
+        this.elaboracion = elaboracion;
+        this.duracion = duracion;
+        this.ingredientes= new Ingredientes[canidadIngredientes];
+    }
+
+
+    //getters y setters generados automaticamente
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getElaboracion() {
+        return elaboracion;
+    }
+
+    public void setElaboracion(String elaboracion) {
+        this.elaboracion = elaboracion;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public Optional<String> añadirIngrediente(Ingredientes ingrediente){
+        for (int i = 0; i < ingredientes.length; i++) {
+            if(ingredientes[i]==null){
+                ingredientes[i]=ingrediente;
+                return Optional.of(ingredientes[i].toString());
+            }
+        }
+        return Optional.of("No se pueden añadir mas ingredientes");
+
+    }
+
+    public Optional<String> eliminarIngrediente(String nombreIngrediente){
+        for (int i = 0; i < ingredientes.length; i++) {
+            if(ingredientes[i]!=null && ingredientes[i].getNombre().equals(nombreIngrediente)){
+                ingredientes[i]=null;
+                return Optional.of("Ingrediente Eliminado "+nombreIngrediente);
+            }
+        }
+        return Optional.of("Ingrediente no encontrado");
+    }
+
+    public void mostrarReceta() {
+        System.out.println( "Receta [nombre=" + nombre + ", elaboracion=" + elaboracion + ", duracion=" + duracion+"]");
+        for (Ingredientes ingredientes2 : ingredientes) {
+            System.out.println(ingredientes2);
+        }
+    }
+    
 }
